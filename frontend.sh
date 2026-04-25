366f48026514426487a1b6eee2b7faa06dc5ec6d
cekharchandra-devops <cekharchandra.devops@gmail.com>
2026-04-25 12:01:35 +0530

    remove existing files

--- frontend.sh
+++ frontend.sh
@@ -21,6 +21,7 @@ dnf install -y nodejs
 # Download the frontend application code, build it, and copy the built files to the Nginx web server directory
 
 curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip
+rm -rf /tmp/frontend
 mkdir -p /tmp/frontend && cd /tmp/frontend
 unzip /tmp/frontend.zip
 npm install
